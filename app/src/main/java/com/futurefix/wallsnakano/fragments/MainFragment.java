04de50ae2b520ea2d36667f6650f087a29ba98e8
DanCrk <dancrkyt@gmail.com>
2021-05-23 00:23:19 -0500

    Creacion del Toolbar junto al menu lateral

---
+++ app/src/main/java/com/futurefix/wallsnakano/fragments/MainFragment.java
@@ -0,0 +1,27 @@
+package com.futurefix.wallsnakano.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.futurefix.wallsnakano.R;
+
+
+public class MainFragment extends Fragment {
+
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        // Inflate the layout for this fragment
+        return  view;
+    }
+}
