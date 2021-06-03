c45d51f2b321e553bd27ff29f6a1296b82ec01c9
DanCrk <dancrkyt@gmail.com>
2021-06-03 12:48:27 -0500

    pantalla completa en el SplashActivity.java

--- app/src/main/java/com/futurefix/wallsnakano/SplashActivity.java
+++ app/src/main/java/com/futurefix/wallsnakano/SplashActivity.java
@@ -3,11 +3,13 @@ package com.futurefix.wallsnakano;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.Handler;
 import android.provider.Settings;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
@@ -20,6 +22,8 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().getDecorView().setSystemUiVisibility( View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
         setContentView(R.layout.activity_splash);
 
         // Agregar animaciones
