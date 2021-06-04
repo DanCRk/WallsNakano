5e3291ca6d5d046b533bba3ac14368ba37e32e55
DanCrk <dancrkyt@gmail.com>
2021-06-04 00:12:39 -0500

    algo hice pero njo me acuerdo

--- app/src/main/java/com/futurefix/wallsnakano/ConfigActivity.java
+++ app/src/main/java/com/futurefix/wallsnakano/ConfigActivity.java
@@ -10,12 +10,15 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.view.View;
 import android.widget.CheckBox;
 import android.widget.TextView;
 
 public class ConfigActivity extends AppCompatActivity {
 
     CheckBox mCheckBox;
+    boolean estadoAviso = false;
+    TextView aviso;
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
@@ -36,9 +39,15 @@ public class ConfigActivity extends AppCompatActivity {
             }
         });
 
-        TextView aviso = findViewById(R.id.Aviso);
+        aviso = findViewById(R.id.Aviso);
 
-        aviso.setOnClickListener(v -> setContentView(R.layout.aviso_activity));
+        aviso.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.aviso_activity);
+                estadoAviso = true;
+            }
+        });
 
         TextView notis = findViewById(R.id.Notification);
 
@@ -47,14 +56,17 @@ public class ConfigActivity extends AppCompatActivity {
         TextView app = findViewById(R.id.AppConfig);
 
         app.setOnClickListener(v -> startActivity(new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)));
-
-
     }
 
     @Override
     public void onBackPressed() {
-        Estado.guardarEstado(mCheckBox.isChecked());
-        finish();
+        if (estadoAviso){
+            setContentView(R.layout.activity_config);
+            estadoAviso = !estadoAviso;
+        }else {
+            Estado.guardarEstado(mCheckBox.isChecked());
+            finish();
+        }
     }
 
     @Override
